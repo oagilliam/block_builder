772726179f6cc5d42e38d4309f72238552b849eb
Olivia Gilliam <75933763+oagilliam@users.noreply.github.com>
2023-05-07 17:14:39 -0400

    init

---
+++ block_builder.py
@@ -0,0 +1,8 @@
+# DESCRIPTION:
+# Write a class Block that creates a block (Duh..)
+
+#Requirements/;
+#The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] 
+#from which the Block should be created.
+
+# Date: 5/07/23
